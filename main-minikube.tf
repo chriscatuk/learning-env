b78d34ad682d64a4e864a0ffef2b78bc2915ae04
Christian Gonzalvez <32960017+chriscatuk@users.noreply.github.com>
2022-03-21 15:53:46 +0000

    sg

--- main-minikube.tf
+++ main-minikube.tf
@@ -4,13 +4,13 @@ module "minikube" {
   enabled = var.minikube
 
   subnet_id         = aws_subnet.a.id
-  sg_ids            = []
+  sg_ids            = [aws_security_group.minikube.id]
   region            = var.region
   hostname          = "training-minikube-${var.suffix_hostname}"
   route53_zoneID    = var.route53_zoneID
   dnsupdate_rolearn = var.dnsupdate_rolearn
   dnsupdate_region  = var.dnsupdate_region
-  instance_type     = "t3.micro"
+  instance_type     = "t3.medium"
   template_path     = "${path.module}/templates/minikube-user_data.tpl"
   template_vars = {
     hostname = module.minikube.hostname
